8cead88cd50b590d61668d812bf8c374fc97cbe8
Adam <129908610+adampdxdotcom@users.noreply.github.com>
2025-11-02 08:40:43 -0800

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Plugin Initializer (DEBUGGING - Step 1)
+ * Plugin Initializer (DEBUGGING - Step 2)
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-// Load the menu file (we know this works)
+// Load the known good files
 require_once TW_PLAYS_PATH . 'admin/menu.php';
+require_once TW_PLAYS_PATH . 'admin/dashboard-page.php';
 
 // ADDING THIS FILE BACK
-require_once TW_PLAYS_PATH . 'admin/dashboard-page.php';
+require_once TW_PLAYS_PATH . 'admin/list-tables/play-columns.php';
